10c87d72f32b7af5b7beddbc846a3f880dbc8ab6
RomaFedoro <fedorovroman1506@gmail.com>
2021-12-18 01:04:45 +0700

    v1.1: choice level, reset button, num stylization

--- game.js
+++ game.js
@@ -1,21 +1,84 @@
+// Список уровней
+const levels = [
+  {
+    id: 1,
+    name: "Легкий",
+    width: 8,
+    height: 8,
+    mines: 10,
+  }, 
+  {
+    id: 2,
+    name: "Средний",
+    width: 18,
+    height: 14,
+    mines: 40,
+  }, 
+  {
+    id: 3,
+    name: "Сложный",
+    width: 30,
+    height: 16,
+    mines: 99,
+  }, 
+]; 
+
+// Параметры игры
 let field = [];
 let listCells = [];
-
 let processGame = false;
-let time = 0;
-let outputLeftDemine = document.getElementById('left_demine').querySelector('.data_text');;
-let outputLeft = document.getElementById('left_cells').querySelector('.data_text');;
-let outputTime = document.getElementById('time').querySelector('.data_text');
-
-let widthField = 18;
-let heightField = 14;
-let numMines = 40;
-
+let nameLevel = "Легкий";
+let widthField = 8;
+let heightField = 8;
+let numMines = 10;
 let left = widthField * heightField;
 let leftDemine = numMines;
+// let time = 0;
+
+// Блоки для вывода в mines.html
+let outputLevel = document.getElementsByClassName('level')[0];
+let outputLeftDemine = document.getElementById('left_demine').querySelector('.data_text');
+let outputLeft = document.getElementById('left_cells').querySelector('.data_text');
+let outputPopupCont = document.getElementsByClassName('popup_cont')[0];
+outputPopupCont.style.display = 'none';
+let btnReset = document.getElementsByClassName('btn_restart')[0];
+btnReset.onclick = function () {
+  generateField();
+  return;
+}
+let btnPopupReset = document.createElement('div');
+btnPopupReset.classList.add('popup_el', 'main_popup_button');
+btnPopupReset.textContent = "Заново"; 
+btnPopupReset.onclick = function () {
+  generateField();
+  closePopup();
+  return;
+}
+
+// let outputTime = document.getElementById('time').querySelector('.data_text');
+
+
+function settingParams(id) {
+  let level = levels.find(lvl => lvl.id === id);
+  nameLevel = level.name;
+  widthField = level.width;
+  heightField = level.height;
+  numMines = level.mines;
+  left = widthField * heightField;
+  leftDemine = numMines;
+}
 
 function generateField() {
+  field = [];
+  listCells = [];
+  processGame = false;
+  left = widthField * heightField;
+  leftDemine = numMines;
+
   let outputField = document.getElementsByClassName('field')[0];
+  while (outputField.firstChild) {
+    outputField.firstChild.remove();
+  }
   for(let i = 0; i < heightField; i++){
     let row = [];
     let outputRow = document.createElement('div');
@@ -48,6 +111,7 @@ function generateField() {
   listCells = outputField.querySelectorAll('.row .cell');
   outputLeftDemine.textContent = leftDemine;
   outputLeft.textContent = left;
+  outputLevel.textContent = nameLevel;
 }
 
 function randomIndex(max) {
@@ -80,10 +144,9 @@ function definingRoles(openCell) {
         row: row,
         column: column,
       });
-      listCells[giveIdCell(row, column)].classList.add("cheat");
+      // listCells[giveIdCell(row, column)].classList.add("cheat");
     }
   }
-  console.log(indexMines);
   for(let id = 0; id < numMines; id++){
     let mine = indexMines[id];
     for(let i = mine.row - 1; i <= mine.row + 1; i++){
@@ -115,6 +178,8 @@ function openCell(id) {
   listCells[id].classList.add("cell-opened");
   if(cell.role) {
     listCells[id].textContent = cell.role;
+    let nameCellRole =  "cell_" + cell.role;
+    listCells[id].classList.add(nameCellRole);
   } else {
     for(let i = row - 1; i <= row + 1; i++){
       for(let j = column - 1; j <= column + 1; j++){
@@ -164,6 +229,7 @@ function gameOver(id) {
     }
   }
   processGame = false;
+  setTimeout(openPopup, 500, generationPopup("Вы проиграли", [btnPopupReset]));
   return true;
 }
 
@@ -182,8 +248,109 @@ function gameWin() {
   }
   leftDemine = numMines;
   processGame = false;
+  setTimeout(openPopup, 500, generationPopup("Вы выиграли", [btnPopupReset]));
   return true;
 }
 
+function iAmLoser() {
+  // Для умников, которые лазят в код
+  for(let i = 0; i < heightField; i++){
+    for(let j = 0; j < widthField; j++){
+      if(field[i][j].role == -1) {
+        listCells[giveIdCell(i, j)].classList.add("cheat");
+      }
+    }
+  }
+
+}
+
+
+// Функции для создания и работы с попапом
+function generationPopup(title, content=[]) {
+  let outputPopup = document.createElement('div');
+  outputPopup.classList.add('popup');
+  let outputPopupHead = document.createElement('div');
+  outputPopupHead.classList.add('popup_el', 'popup_header');
+  let outputTitle = document.createElement('div');
+  outputTitle.classList.add('header_text');
+  outputTitle.textContent = title;
+  let closeBtn = document.createElement('div');
+  closeBtn.classList.add('close_icon');
+  closeBtn.onclick = function () {
+    closePopup();
+    return;
+  }
+
+  outputPopupHead.appendChild(outputTitle);
+  outputPopupHead.appendChild(closeBtn);
+  outputPopup.appendChild(outputPopupHead);
+  for(let i=0; i < content.length; i++) {
+    outputPopup.appendChild(content[i]);
+  }
+
+  return outputPopup;
+}
+
+function endingWord(value) {
+  value = Math.abs(value);
+  if((value > 10) && (value < 20)) {
+    return "мин";
+  } else {
+    value = value % 10;
+    if((value == 0) || (value > 4)) return "мин";
+    if(value == 1) return "минa";
+    return "мины";
+  }
+}
+
+function openPopup(popup) { 
+  while (outputPopupCont.firstChild) {
+    outputPopupCont.firstChild.remove();
+  }
+  outputPopupCont.appendChild(popup);
+  outputPopupCont.style.display = 'flex';
+}
+
+function closePopup() { 
+  while (outputPopupCont.firstChild) {
+    outputPopupCont.firstChild.remove();
+  }
+  outputPopupCont.style.display = 'none';
+}
+
+
+// Создание блока управления уровнем сложности
+let popupLevelsEl = document.createElement('div');
+popupLevelsEl.classList.add('popup_el', 'popup_level_cont');
+for(let i=0; i < levels.length; i++) {
+  let level = levels[i];
+  let popupLevelEl = document.createElement('div');
+  popupLevelEl.classList.add('popup_level');
+  popupLevelEl.onclick = function () {
+    settingParams(level.id);
+    generateField();
+    closePopup();
+    return;
+  }
+  let popupLevelName = document.createElement('div');
+  popupLevelName.classList.add('popup_level_title');
+  popupLevelName.textContent = level.name;
+  let descriptionLevel = "поле " + level.width + "x" + level.height + ", " + level.mines + " " + endingWord(level.mines);
+  let popupLevelDescript = document.createElement('div');
+  popupLevelDescript.classList.add('popup_level_descript');
+  popupLevelDescript.textContent = descriptionLevel;
+  popupLevelEl.appendChild(popupLevelName);
+  popupLevelEl.appendChild(popupLevelDescript);
+  popupLevelsEl.appendChild(popupLevelEl);
+}
+let popupLevels = generationPopup("Выберите уровень сложности", [popupLevelsEl]);
+let btnPopupLevels = document.getElementsByClassName('level')[0];
+btnPopupLevels.onclick = function () {
+  openPopup(popupLevels);
+  return;
+}
+
+
+settingParams(2);
 generateField();
 
